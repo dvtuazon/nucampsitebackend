83380d4fa515c22c181cd63d64305287564b8f8a
David <dvtuazon@gmail.com>
2020-10-29 19:06:01 -0700

    sessions pt2

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const { TooManyRequests } = require('http-errors');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    admin: {
+        type: Boolean,
+        default: false
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
