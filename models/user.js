51e2d3ebfeb1061f5e42f247d70fc55fb81d0127
David <dvtuazon@gmail.com>
2020-10-29 23:05:10 -0700

    authentication with passport

--- models/user.js
+++ models/user.js
@@ -1,21 +1,16 @@
 const { TooManyRequests } = require('http-errors');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
     admin: {
         type: Boolean,
         default: false
     }
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', userSchema);
